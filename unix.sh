273a11cc415e7acd914f6983022714af02e2c079
Artem Aleksashkin <artem#aleksashkin.com>
2016-03-01 20:45:01 +0300

    done

---
+++ unix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+find . -name '.svn' -type d -exec rm -rf {} \;
+$ free
+             total       used       free     shared    buffers     cached
+Mem:      10198876   10053796     145080          0     139396    6364396
+-/+ buffers/cache:    3550004    6648872
+Swap:     10485756       5412   10480344
